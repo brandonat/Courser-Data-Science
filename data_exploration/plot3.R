bc3f2cd2a2f30bfff1110940663bd288c699a5e7
Brandon Taylor <brandonat@gmail.com>
2016-06-20 09:48:29 -0400

    new plot3

--- data_exploration/plot3.R
+++ data_exploration/plot3.R
@@ -1,4 +1,4 @@
-## Plot 2
+## Plot 3
 
 ## Ojective
 ## --------------------------------------------------------------
@@ -9,6 +9,7 @@
 ## Use the ggplot2 plotting system to make a plot answer this question.
 
 library(ggplot2)
+library(dplyr)
 
 ## Note: data provided by course instructor, but can be reproduced
 ## using the source listed in README.md
@@ -17,3 +18,16 @@ library(ggplot2)
 nei <- readRDS("summarySCC_PM25.rds")
 scc <- readRDS("Source_Classification_Code.rds")
 
+df <- nei %>% 
+  filter(fips == "24510") %>%
+  group_by(year, type) %>%
+  summarise(total = sum(Emissions))
+
+png(filename = "plot3.png")
+ggplot(df, aes(x = factor(year), y = total)) + 
+  geom_bar(stat = "identity", fill = "blue", alpha = 0.75) + 
+  facet_wrap(~type) + 
+  labs(x = "Year", y = "Tons of PM2.5",
+       title = "Total Emissions by Year and Type\nBaltimore City, Maryland") +
+  theme_bw()
+dev.off()
